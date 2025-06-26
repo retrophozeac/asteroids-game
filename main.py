e24aa50ae965d8ce69ef7e767c17ddab5f2c395e
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-06-26 13:52:47 +0530

    added player ship and rotation

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
